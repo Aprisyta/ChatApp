fd0c5d72a95c1246d591290dd0664e50b65a5885
Aprisyta <aprisyta10@gmail.com>
2018-06-16 01:35:27 +0530

    Send button added

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,32 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
-
   final TextEditingController _textController = new TextEditingController();
 
   Widget _buildTextComposer() {
     return new Container(
       margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: new InputDecoration.collapsed(hintText: "Send a message"),
+      child: new Row(
+        children: <Widget>[
+          new Flexible(
+            child: new TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration:
+                  new InputDecoration.collapsed(hintText: "Send a message"),
+            ),
+          ),
+          new Container(
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),
+            child: new IconButton(
+                icon: new Icon(
+                    Icons.send,
+                  color: Colors.blue,
+                ),
+                onPressed: () => _handleSubmitted(_textController.text)
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -40,7 +56,7 @@ class ChatScreenState extends State<ChatScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return  new Scaffold(
+    return new Scaffold(
       appBar: new AppBar(
         title: new Text("Friendlychat"),
       ),
